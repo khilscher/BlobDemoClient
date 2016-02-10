d5395bdc6d08bd39c3adeac9fc14ead969042a3f
khilscher <kevin@nospam.ca>
2016-02-10 11:10:07 -0700

    Initial Commit

---
+++ BlobDemoClient/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace BlobDemoClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.txtBlobURL = new System.Windows.Forms.TextBox();
+            this.lblStorageURL = new System.Windows.Forms.Label();
+            this.txtContainerName = new System.Windows.Forms.TextBox();
+            this.txtBlobName = new System.Windows.Forms.TextBox();
+            this.lblContainerName = new System.Windows.Forms.Label();
+            this.lblBlobName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(62, 364);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(94, 35);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(62, 85);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(849, 26);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(937, 81);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(98, 35);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(58, 62);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(107, 20);
+            this.lblFilePath.TabIndex = 3;
+            this.lblFilePath.Text = "File to Upload";
+            // 
+            // txtBlobURL
+            // 
+            this.txtBlobURL.AcceptsTab = true;
+            this.txtBlobURL.Location = new System.Drawing.Point(62, 159);
+            this.txtBlobURL.Name = "txtBlobURL";
+            this.txtBlobURL.Size = new System.Drawing.Size(849, 26);
+            this.txtBlobURL.TabIndex = 4;
+            this.txtBlobURL.Text = "http://khblobdemoclient.blob.core.windows.net/";
+            // 
+            // lblStorageURL
+            // 
+            this.lblStorageURL.AutoSize = true;
+            this.lblStorageURL.Location = new System.Drawing.Point(58, 136);
+            this.lblStorageURL.Name = "lblStorageURL";
+            this.lblStorageURL.Size = new System.Drawing.Size(103, 20);
+            this.lblStorageURL.TabIndex = 5;
+            this.lblStorageURL.Text = "Storage URL";
+            // 
+            // txtContainerName
+            // 
+            this.txtContainerName.Location = new System.Drawing.Point(62, 235);
+            this.txtContainerName.Name = "txtContainerName";
+            this.txtContainerName.Size = new System.Drawing.Size(316, 26);
+            this.txtContainerName.TabIndex = 6;
+            this.txtContainerName.Text = "test1";
+            // 
+            // txtBlobName
+            // 
+            this.txtBlobName.Location = new System.Drawing.Point(62, 308);
+            this.txtBlobName.Name = "txtBlobName";
+            this.txtBlobName.Size = new System.Drawing.Size(316, 26);
+            this.txtBlobName.TabIndex = 7;
+            // 
+            // lblContainerName
+            // 
+            this.lblContainerName.AutoSize = true;
+            this.lblContainerName.Location = new System.Drawing.Point(62, 212);
+            this.lblContainerName.Name = "lblContainerName";
+            this.lblContainerName.Size = new System.Drawing.Size(124, 20);
+            this.lblContainerName.TabIndex = 8;
+            this.lblContainerName.Text = "Container Name";
+            // 
+            // lblBlobName
+            // 
+            this.lblBlobName.AutoSize = true;
+            this.lblBlobName.Location = new System.Drawing.Point(62, 285);
+            this.lblBlobName.Name = "lblBlobName";
+            this.lblBlobName.Size = new System.Drawing.Size(130, 20);
+            this.lblBlobName.TabIndex = 9;
+            this.lblBlobName.Text = "Block Blob Name";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1123, 452);
+            this.Controls.Add(this.lblBlobName);
+            this.Controls.Add(this.lblContainerName);
+            this.Controls.Add(this.txtBlobName);
+            this.Controls.Add(this.txtContainerName);
+            this.Controls.Add(this.lblStorageURL);
+            this.Controls.Add(this.txtBlobURL);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Block Blob Demo Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.TextBox txtBlobURL;
+        private System.Windows.Forms.Label lblStorageURL;
+        private System.Windows.Forms.TextBox txtContainerName;
+        private System.Windows.Forms.TextBox txtBlobName;
+        private System.Windows.Forms.Label lblContainerName;
+        private System.Windows.Forms.Label lblBlobName;
+    }
+}
+
---
+++ BlobDemoClient/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Added references
+using System.IO;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+
+//Author: Kevin Hilscher
+//Add NuGet package 'WindowsAzure.Storage'
+//Review 'Get started with Azure Blob storage using .Net' at 
+//https://github.com/Azure/azure-content/blob/master/articles/storage/storage-dotnet-how-to-use-blobs.md
+
+namespace BlobDemoClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtFilePath.Text))
+            {
+                MessageBox.Show("Please select a file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Retrieve storage account connection string from app.config.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference(txtContainerName.Text);
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            // Retrieve reference to a blob 
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(txtBlobName.Text);
+
+            // Create or overwrite the blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(txtFilePath.Text))
+            {
+                try
+                {
+                    //TODO: Add better error handling
+                    blockBlob.UploadFromStream(fileStream);
+                    MessageBox.Show("Upload Complete", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not upload file. Error: " + ex.Message);
+                }
+            }
+
+            
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            txtFilePath.Text = openFileDialog1.FileName;
+                            txtBlobName.Text = openFileDialog1.SafeFileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not read file from disk. Error: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
